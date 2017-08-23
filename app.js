3842f21f563cb23e802c8a7df0600982ce8491f2
gabietaylor <gabie.taylor@yahoo.com>
2017-08-23 16:44:05 -0400

    init firebase

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
 console.log("Namaste");
+// Init Firebase
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyC1GTHMmvJ97d31KS2KYOmfu_rrK068Axo",
+    authDomain: "fir-chatpractice-78d23.firebaseapp.com",
+    databaseURL: "https://fir-chatpractice-78d23.firebaseio.com",
+    projectId: "fir-chatpractice-78d23",
+    storageBucket: "fir-chatpractice-78d23.appspot.com",
+    messagingSenderId: "1075006880834"
+  };
+  firebase.initializeApp(config);
